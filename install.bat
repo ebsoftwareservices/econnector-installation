ceece9d0139a5922da0e947deb79e817ae790561
DamnOps <damnops.gg@gmail.com>
2024-05-15 21:46:52 +0800

    updated install bat

--- install.bat
+++ install.bat
@@ -4,6 +4,14 @@ call config.bat
 
 set INSTALL_HOME=C:\%SERVICE_NAME%
 
+REM install java
+if not exist jdk.zip (
+powershell -command "Start-BitsTransfer -Source https://download.java.net/java/GA/jdk21.0.2/f2283984656d49d69e91c558476027ac/13/GPL/openjdk-21.0.2_windows-x64_bin.zip -Destination jdk.zip"
+)
+
+powershell -command "Expand-Archive jdk.zip %INSTALL_HOME%"
+set PR_JVM=%INSTALL_HOME%\jdk-21.0.2\bin\server\jvm.dll
+
 REM Service log configuration
 set PR_LOGPREFIX=%SERVICE_NAME%
 set PR_LOGPATH=%INSTALL_HOME%\logs
@@ -12,13 +20,9 @@ set PR_STDERROR=auto
 set PR_LOGLEVEL=Info
 set PR_DESCRIPTION=%SERVICE_NAME%
 
-REM Path to java installation
-REM https://download.java.net/java/GA/jdk21.0.2/f2283984656d49d69e91c558476027ac/13/GPL/openjdk-21.0.2_windows-x64_bin.zip
-set PR_JVM=%INSTALL_HOME%\jdk-21\bin\server\jvm.dll
+REM Startup configuration
 set PR_INSTALL=%INSTALL_HOME%\prunsrv.exe
 set PR_CLASSPATH=%INSTALL_HOME%\%CLASS_FILE%
- 
-REM Startup configuration
 set PR_STARTUP=auto
 set PR_STARTMODE=jvm
 set PR_STARTCLASS=com.ebsoftwareservices.econnector.daemon.EconnectorDaemonOnWindows
@@ -31,7 +35,7 @@ set PR_STOPCLASS=com.ebsoftwareservices.econnector.daemon.EconnectorDaemonOnWind
 set PR_STOPMETHOD=windowsService
 set PR_STOPPARAMS=stop
 
-REM Install servic 
+REM Install service
 mkdir "%PR_LOGPATH%" >NUL 2>&1
 xcopy /E . "%INSTALL_HOME%" >NUL 2>&1
 "%INSTALL_HOME%\prunsrv.exe" //DS//%SERVICE_NAME% >NUL 2>&1
